eefe050bbc4dd84af42f7bed72dbe44bdc0c83da
Trollfjorden <244750006+Trollfjorden@users.noreply.github.com>
2026-04-27 03:18:03 +0300

    Revert

--- ct/riventstest.sh
+++ ct/riventstest.sh
@@ -64,7 +64,7 @@ function update_script() {
     cd /opt/rivents.build
     $STD pnpm install --frozen-lockfile --force
     $STD pnpm turbo telemetry disable
-    $STD pnpm turbo run build --no-daemon --filter=@repo/riven#build
+    $STD pnpm turbo run build --no-daemon --filter=@repo/riven
 
     msg_info "Backing up Data"
     cp /opt/rivents/.env.riven /tmp/.env.riven 2>/dev/null || true
