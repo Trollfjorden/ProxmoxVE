1e2d97b8a94abe2f0d2b94b665898cc7d3a09410
Trollfjorden <244750006+Trollfjorden@users.noreply.github.com>
2026-06-23 14:10:03 +0300

    fix: change source url to feature branch

--- ct/llama-cpp.sh
+++ ct/llama-cpp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/Trollfjorden/ProxmoxVE/feature/Riven/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: Trollfjorden
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
